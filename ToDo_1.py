e9356a944b713e36a3d954c912623ca6e53dbfa3
Brent Lacy <brent.lacy@gmail.com>
2016-09-07 20:54:28 -0400

    still trying to get it to load the gui page

--- ToDo_1.py
+++ ToDo_1.py
@@ -4,18 +4,18 @@ from PyQt4 import QtCore, QtGui, uic
 
 form_class = uic.loadUiType("todo_menu.ui")[0]
 
-class ToDoMenu(QtGui.QMainWindow, form_class):
+class ToDoMenuClass(QtGui.QMainWindow, form_class):
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self, parent)
         self.setupUi(self)
-        self.Add_ToDo_item_button.clicked.connect(self.Add_ToDo_button_clicked)
+        self.add_ToDo_item_button.clicked.connect(self.add_ToDo_button_clicked)
         self.actionExit.triggered.connect(self.menuExit_selected)
-    def Add_ToDo_button_clicked(self):
+    def add_ToDo_button_clicked(self):
         todo_Item = str(self.ToDo_Add_Item())
         
         
 app = QtGui.QApplication(sys.argv)
-myapp = ToDoMenu()
+myapp = ToDoMenuClass()
 myapp.show()
 app.exec_()
     
