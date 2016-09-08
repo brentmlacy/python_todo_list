42196002dbde68526fdac9f1cf60d161dbd360e9
Brent Lacy <brent.lacy@gmail.com>
2016-09-08 18:52:36 -0400

    still working on fixing

--- ToDo_1.py
+++ ToDo_1.py
@@ -8,10 +8,10 @@ class ToDoMenuClass(QtGui.QMainWindow, form_class):
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self, parent)
         self.setupUi(self)
-        self.add_ToDo_item_button.clicked.connect(self.add_ToDo_button_clicked)
+        self.addTodobtn.clicked.connect(self.addTodobtn_clicked)
         self.actionExit.triggered.connect(self.menuExit_selected)
-    def add_ToDo_button_clicked(self):
-        todo_Item = str(self.ToDo_Add_Item())
+    def addTodobtn_clicked(self):
+        todo_Item = str(self.ToDoAddItem())
         
         
 app = QtGui.QApplication(sys.argv)
